aa7c559bb5a28fc72dc53d09dfbcbece3d7a80a6
nguyensongnghiem <nguyensongnghiem@gmail.com>
2024-03-06 21:23:41 +0700

    update cau 22

--- casestudy/new_furama.sql
+++ casestudy/new_furama.sql
@@ -1,4 +1,4 @@
-create database if not exists furama;
+create database if not exists newfurama;
 -- Tạo bảng vi_tri
 create table vi_tri (
 	ma_vi_tri INT  primary key auto_increment ,
@@ -23,9 +23,12 @@ create table nhan_vien (
     so_dien_thoai varchar(45),
     email varchar(45),
     dia_chi varchar(45),
-    ma_vi_tri int references vi_tri(ma_vi_tri),
-    ma_trinh_do int references trinh_do(ma_trinh_do),
-    ma_bo_phan int references bo_phan(ma_bo_phan)
+    ma_vi_tri int ,
+    ma_trinh_do int ,
+    ma_bo_phan int ,
+    foreign key (ma_vi_tri) references vi_tri(ma_vi_tri),
+    foreign key (ma_trinh_do) references  trinh_do(ma_trinh_do),
+    foreign key (ma_bo_phan) references bo_phan(ma_bo_phan)
     
 );
 
@@ -42,14 +45,15 @@ create table loai_khach (
 );
 create table khach_hang (
 	ma_khach_hang int primary key auto_increment ,
-    ma_loai_khach int references loai_khach(ma_loai_khach),
+    ma_loai_khach int ,
 	ho_ten varchar(45),
     ngay_sinh date,
     gioi_tinh bit(1),
     so_cmnd varchar(45),
     so_dien_thoai varchar(45),
     email varchar(45),
-    dia_chi varchar(45)
+    dia_chi varchar(45),
+    foreign key(ma_loai_khach) references loai_khach(ma_loai_khach)
 );
 
 create table loai_dich_vu (
@@ -66,28 +70,35 @@ create table dich_vu (
     dien_tich int,
     chi_phi_thue double,
     so_nguoi_toi_da int,
-    ma_kieu_thue int references kieu_thue(ma_kieu_thue),
-	ma_loai_dich_vu int references loai_dich_vu(ma_loai_dich_vu),
+    ma_kieu_thue int ,
+	ma_loai_dich_vu int ,
     tieu_chuan_phong varchar(45),
     mo_ta_tien_nghi_khac varchar(45),
     dien_tich_ho_boi double,
     so_tang int,
-    dich_vu_mien_phi_di_kem text
+    dich_vu_mien_phi_di_kem text,
+    foreign key (ma_kieu_thue) references kieu_thue(ma_kieu_thue),
+    foreign key (ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu)
 );
 create table hop_dong (
 	ma_hop_dong int primary key auto_increment ,
     ngay_lam_hop_dong datetime,
     ngay_ket_thuc_hop_dong datetime,
     tien_dat_coc double,
-    ma_nhan_vien int references nhan_vien(ma_nhan_vien),
-    ma_khach_hang int references khach_hang(ma_khach_hang),
-    ma_dich_vu int references dich_vu(ma_dich_vu)
+    ma_nhan_vien int ,
+    ma_khach_hang int ,
+    ma_dich_vu int ,
+    foreign key (ma_nhan_vien) references nhan_vien(ma_nhan_vien),
+    foreign key (ma_khach_hang)  references khach_hang(ma_khach_hang),
+    foreign key (ma_dich_vu) references dich_vu(ma_dich_vu)
 ); 
 create table hop_dong_chi_tiet (
 	ma_hop_dong_chi_tiet int primary key auto_increment ,
-    ma_hop_dong int references hop_dong(ma_hop_dong),
+    ma_hop_dong int ,
     so_luong int,
-    ma_dich_vu_di_kem int references dich_vu_di_kem(ma_dich_vu_di_kem)
+    ma_dich_vu_di_kem int,
+    foreign key (ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
+    foreign key (ma_hop_dong) references hop_dong(ma_hop_dong)
 );
 
 
@@ -374,16 +385,6 @@ group by n.ma_nhan_vien)
 ;
 
 -- Câu 17
-select k.ma_khach_hang, k.ho_ten, sum(d.chi_phi_thue) + sum(hc.so_luong * dk.gia) as tong_chi_phi
-from khach_hang k 
-join hop_dong h on h.ma_khach_hang = k.ma_khach_hang
-join hop_dong_chi_tiet hc on hc.ma_hop_dong = h.ma_hop_dong
-join dich_vu d on d.ma_dich_vu = h.ma_dich_vu
-join dich_vu_di_kem dk  on dk.ma_dich_vu_di_kem = hc.ma_dich_vu_di_kem
--- where year(h.ngay_lam_hop_dong) = 2021
--- group by k.ma_khach_hang
--- having tong_chi_phi>10000000
-;
 
 update khach_hang
 set khach_hang.ma_loai_khach = 1
@@ -404,15 +405,81 @@ having cpdk>1000000 and lk.ten_loai_khach ="Platinium") as result on result.ma_k
 );
 
 
--- Câu 19
+-- Câu 18
 alter table hop_dong
-drop constraint ma_khach_hang;
+drop constraint hop_dong_ibfk_2;
+
+alter table hop_dong
+add constraint foreign key (ma_khach_hang) references khach_hang(ma_khach_hang) on delete cascade; 
+
+alter table khach_hang
+drop column available ;
+
+alter table khach_hang
+add column da_xoa bit default 0;
 
 select k.ma_khach_hang
 from khach_hang k
 join hop_dong h on h.ma_khach_hang = k.ma_khach_hang
  where year(h.ngay_lam_hop_dong) <2021
  group by k.ma_khach_hang
-
  ;
+ 
+update khach_hang
+set da_xoa = 1 
+where khach_hang.ma_khach_hang in (select k.ma_khach_hang
+from khach_hang k
+join hop_dong h on h.ma_khach_hang = k.ma_khach_hang
+ where year(h.ngay_lam_hop_dong) <2021
+ group by k.ma_khach_hang
+ );
+ 
+ 
+ -- Câu 19 
+
+ 
+ select dk.ma_dich_vu_di_kem
+ from dich_vu_di_kem dk 
+ join hop_dong_chi_tiet hc on hc.ma_dich_vu_di_kem = dk.ma_dich_vu_di_kem
+ join hop_dong h on h.ma_hop_dong = hc.ma_hop_dong
+ where year(h.ngay_lam_hop_dong) = 2020
+ group by dk.ma_dich_vu_di_kem
+ having sum(so_luong) > 10
+ ;
+ 
+ 
+  update dich_vu_di_kem 
+  set gia = gia * 2
+  where dich_vu_di_kem.ma_dich_vu_di_kem in (select dk.ma_dich_vu_di_kem
+ from dich_vu_di_kem dk 
+ join hop_dong_chi_tiet hc on hc.ma_dich_vu_di_kem = dk.ma_dich_vu_di_kem
+ join hop_dong h on h.ma_hop_dong = hc.ma_hop_dong
+ where year(h.ngay_lam_hop_dong) = 2020
+ group by dk.ma_dich_vu_di_kem
+ having sum(so_luong) > 10);
+ 
+ -- Câu 20 
+ 
+ select n.ma_nhan_vien as id, n.ho_ten,n.email, n.so_dien_thoai, n.ngay_sinh,n.dia_chi
+ from nhan_vien n 
+ union
+  select k.ma_khach_hang, k.ho_ten,k.email, k.so_dien_thoai, k.ngay_sinh,k.dia_chi
+ from khach_hang k ;
+  ;
+ 
+ -- Câu 21
+ 
+ create view v_nhan_vien as
+ select n.ma_nhan_vien, n.ho_ten, h.ngay_lam_hop_dong
+ from nhan_vien n 
+ join hop_dong h on n.ma_nhan_vien = h.ma_nhan_vien
+ where n.dia_chi like "%Đà Nẵng%" and ngay_lam_hop_dong = "2021-06-01"
+ group by n.ma_nhan_vien;
+ 
+ -- Câu 22
+ 
+ update nhan_vien
+ set nhan_vien
+ where replace ("sdf hải châu asdfd ", "hải châu" , "liên chiểu");
+ 
  
